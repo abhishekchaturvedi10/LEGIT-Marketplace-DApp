de740896c4225123ac98b6f306b59ded24a30d3d
Abhishek Chaturvedi <abchaturvedi@expediagroup.com>
2022-07-02 15:22:20 +0530

    Project complete

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Main from './components/MainComponent';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Main />
+            </div>
+        );
+    }
+}
+
+export default App;
